e020f3e543ce379c7fc8f0dc0a9a1f17b5bb4185
jordan <hemantkasat@gmail.com>
2017-01-08 01:48:35 +0530

    player moving

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,32 @@
 var cube, scene, camera, renderer, x;
 var geometry, material, mesh, mesh2, mesh3;
 var group = new THREE.Object3D();
-	
+var projector = new THREE.Projector();
+var mouseVector = new THREE.Vector3();
+var container = document.getElementsByClassName('app')[0]	;
+var containerWidth = container.clientWidth;
+var containerHeight = container.clientHeight;
+console.log(containerHeight);
+
 
 var main = function() {
 	init();
 	render();
 }
 
+function moveup() {
+	group.position.y += 1;
+}
+function movedown() {
+	group.position.y -= 1;
+}
+function moveleft() {
+	group.position.x -= 1;
+
+}
+function moveright() {
+	group.position.x += 1;
+}
 function init() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -37,9 +56,43 @@ function init() {
 	scene.add(group);
 	camera.position.z = 5;
 
+}
+window.addEventListener( 'resize', onWindowResize, false);
+window.addEventListener( 'mousemove', onMouseMove, false );
+
+function onMouseMove(e){
+
+	mouseVector.x = 2 * (e.clientX / containerWidth) - 1;
+    mouseVector.y = 1 - 2 * ( e.clientY / containerHeight );
+
+
+ //    projector.unprojectVector( mouseVector, camera );
+	// var raycaster = new THREE.Raycaster( camera.position, mouseVector.subSelf( camera.position ).normalize() );
+
+	// // create an array containing all objects in the scene with which the ray intersects
+	// var intersects = raycaster.intersectObjects( group.children );
+
+
+ //    var vector = mouseVector.clone().unproject( camera );
+	// var direction = new THREE.Vector3( 0, 0, -1 ).transformDirection( camera.matrixWorld );
+	// var raycaster = new THREE.Raycaster();
+	// raycaster.setFromCamera( mouseVector, camera ); 
+	// raycaster.set( vector, direction );
+	// console.log(group.children);
+	// raycaster.setFromCamera( mouseVector, camera ); 
+	// var intersects = raycaster.intersectObjects(group.children);
+
+    // console.log(intersects);
+
+    // intersection = intersects[0];
+    // obj = intersection.object;
+    // obj.material.color.setRGB( 1.0 - 1/ intersects.length, 0, 0 );
+
+
 
 }
-window.addEventListener( 'resize', onWindowResize, false );
+
+
 
 function onWindowResize(){
 
@@ -54,7 +107,7 @@ function render() {
 	x = (x+20)%200;
 	var s = Math.min(x,200-x);
 	var y = 1.25+s/1000.0, z = 1-s/1000.0;
-	console.log(y,x);
+	// console.log(y,x);
 	group.remove(mesh2);
 	group.remove(mesh3);
 	geometry = new THREE.RingGeometry( z, 1.0, 32 );
