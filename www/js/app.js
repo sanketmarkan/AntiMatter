e0bec17672b01d00be8550219a2a7da48791f932
Aditya Motwani <adityamotwani@gmail.com>
2017-01-07 22:19:57 +0530

    Added Screen Resize

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,16 @@ function init() {
 
 
 }
+window.addEventListener( 'resize', onWindowResize, false );
 
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 function render() {
 	requestAnimationFrame( render );
 
